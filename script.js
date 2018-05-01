e2fbc168cd3edb8706341dfd0ae09bdf3fc957e6
Elvin Morales <elvinmorales48@gmail.com>
2018-05-01 16:50:36 -0600

    agregando conexion online y offline

--- script.js
+++ script.js
@@ -33,15 +33,51 @@ const applicationServerPublicKey = 'BFIuRbWDMmpLOZrMJD6QGdmQ-P1abdQuX7v4z4N58TEL
                     body: 'Hola, soy una notificacion',
                     icon: './img/icon_192x192.png'
                 }
-            )
-            notify.close()
+            )  
         })
     }
 })(document, navigator, window, console.log);
 
 //deteccion del estado de la conexion
 ((d, n, w, c) => {
+    const   header = d.querySelector('.mdl-layout__header'),
+            metaTagTheme = d.querySelector('meta[name=theme-color]')
+    
+    function networkStatus(e){
+        if(n.onLine){
+            metaTagTheme.setAttribute('content', '#F7DF1E')
+            header.classList.remove('u-offline')
+            alert('Conexion recuperada :)')
+            let notify = new Notification('Conexion',{
+                body: 'Conexion recuperada',
+                icon: './img/icon_192x192.png'
+            })
+        }else{
+            metaTagTheme.setAttribute('content', '#666')
+            header.classList.add('u-offline')
+            alert('Conexion perdida :(')
+            let notify = new Notification('Conexion',{
+                body: 'Conexion perdida',
+                icon: './img/icon_192x192.png'
+            })
+        }
+    }
+
+    d.addEventListener('DOMContentLoaded', e=>{
+        if(!n.onLine){
+            networkStatus(this)
+        }
+
+        w.addEventListener('online', networkStatus)
+        w.addEventListener('offline', networkStatus)
+    })
+})(document, navigator, window, console.log);
+
+((d, n, w, c) => {
     
 })(document, navigator, window, console.log);
 
+((d, n, w, c) => {
+    
+})(document, navigator, window, console.log);
 
